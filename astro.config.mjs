ee4c7f09041efbd494ffe2b3974659d3c040fed1
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2025-10-07 15:39:24 +0200

    chore: rehype pretty code setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,31 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
-
+import rehypePrettyCode from "rehype-pretty-code";
 import sitemap from "@astrojs/sitemap";
+import { transformerNotationDiff } from "@shikijs/transformers";
+import theme from "shiki/themes/github-light-default.mjs";
+import { visit } from "unist-util-visit";
+
+/** @type {import('rehype-pretty-code').Options} */
+const rehypePrettyCodeOptions = {
+  theme: theme,
+  keepBackground: false,
+  transformers: [transformerNotationDiff()],
+};
+
+/** @type {() => (tree: any) => void} */
+const plugin = () => (tree) => {
+  visit(tree, (node) => {
+    if (node?.type === "element" && node?.tagName === "figure") {
+      if (!("data-rehype-pretty-code-figure" in node.properties)) {
+        return;
+      }
+
+      node.properties.class = "not-prose";
+    }
+  });
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +34,12 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [rehypePrettyCode, rehypePrettyCodeOptions],
+      [plugin, {}],
+    ],
   },
 });
